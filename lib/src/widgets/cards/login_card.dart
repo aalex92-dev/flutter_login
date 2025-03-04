06d62f431de3b3a453c048763862bd869f2858e0
aalex <shadlolz@shadlolz.ru>
2025-03-04 12:07:54 +0500

    Init.

--- lib/src/widgets/cards/login_card.dart
+++ lib/src/widgets/cards/login_card.dart
@@ -711,8 +711,6 @@ class _LoginCardState extends State<_LoginCard> with TickerProviderStateMixin {
                   if (widget.introWidget != null) widget.introWidget!,
                   _buildUserField(textFieldWidth, messages, auth),
                   const SizedBox(height: 20),
-                  _buildPasswordField(textFieldWidth, messages, auth),
-                  const SizedBox(height: 10),
                 ],
               ),
             ),
